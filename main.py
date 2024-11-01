a4350a9a4f1cdefeed7c32a6df6bf4452cba82d9
24-python <24_python@mail.ru>
2024-11-01 19:25:59 +0300

    Добавленные комментарии в код работы с гет запросами

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-import requests
-import pprint
+import requests  # Импортируем модуль requests для работы с HTTP-запросами
+import pprint  # Импортируем модуль pprint для красивого вывода данных
 
+# Отправляем GET-запрос на указанный URL и сохраняем ответ в переменной response
 response = requests.get('https://api.github.com')
+
+# Распечатываем статусный код ответа
 # print(response.status_code)
-#
+
+# Проверяем, успешно ли выполнен запрос
 # if response.ok:
-#     print("Запрос успешно выполнен")
+#     print("Запрос успешно выполнен")  # Если да, выводим сообщение об успехе
 # else:
-#     print("Запрос не удался")
+#     print("Запрос не удался")  # Если нет, выводим сообщение о неудаче
 
-#print(response.text)
+# Выводим текст ответа
+# print(response.text)
 
-#print(response.content)
+# Выводим содержимое ответа в формате байтов
+# print(response.content)
 
-#print(response.json)
+# Выводим JSON-объект, полученный из ответа
+# print(response.json)
 
+# Преобразуем ответ в JSON-формат и сохраняем в переменной response_json
 response_json = response.json()
-#print(response_json)
 
+# Выводим JSON-объект в удобочитаемом формате с помощью pprint
 pprint.pprint(response_json)
--- primer-raboty.py
+++ primer-raboty.py
@@ -1,12 +1,19 @@
-import requests
-import pprint
+import requests  # Импортируем модуль requests для выполнения HTTP-запросов
+import pprint    # Импортируем модуль pprint для форматированного вывода данных
 
+# Создаем словарь с параметрами запроса, где 'q' — это параметр поиска
 params = {
-    'q': 'JavaScript'
-    }
+    'q': 'JavaScript'  # Ищем репозитории, связанные с JavaScript
+}
 
+# Отправляем GET-запрос к API GitHub для поиска репозиториев с указанными параметрами
 response = requests.get('https://api.github.com/search/repositories', params=params)
-response_json = response.json()
-#pprint.pprint(response_json)
 
+# Преобразуем ответ в JSON-формат и сохраняем в переменную response_json
+response_json = response.json()
+
+# Закомментированная строка для вывода полного JSON-ответа в удобочитаемом виде
+# pprint.pprint(response_json)
+
+# Выводим количество найденных репозиториев, используя значение из JSON-ответа
 print(f'Количество репозиториев: {response_json["total_count"]}')
